93559710f1dafa977968b09708cff0069e9cba20
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2022-07-18 14:05:22 +0300

    done from functionality

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 // Cube
+let x = 0;
 let y = 0;
+let z = 0
 const cube = document.querySelector('.cube');
+document.querySelector('.top-x-control').addEventListener('click',()=> {
+    cube.style.transform = `rotateX(${x += 20}deg) rotateZ(${z}deg)`
+});
+document.querySelector('.bottom-x-control').addEventListener('click',()=> {
+    cube.style.transform = `rotateX(${x -= 20}deg) rotateZ(${z}deg)`
+});
+
+document.querySelector('.left-y-control').addEventListener('click',()=> {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y -= 20}deg) rotateZ(${z}deg) `
+});
+document.querySelector('.right-y-control').addEventListener('click',()=> {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y += 20}deg) rotateZ(${z}deg) `
+});
+document.querySelector('.top-z-control').addEventListener('click',()=> {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z -= 20}deg) `
+});
+document.querySelector('.bottom-z-control').addEventListener('click',()=> {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z +=20}deg) `
+});
+
+
 const playPause =()=> {
     setInterval(() =>{
 
@@ -9,7 +32,9 @@ const playPause =()=> {
     },100)
 }
 
-playPause();
+/* playPause(); */
+
+
 // End of cube
 
 //Slideshow
